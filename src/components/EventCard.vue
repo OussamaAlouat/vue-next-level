0ba3153b2fead3c8882e2ab350bef491e20e2a14
oussamaAlouat <al317257@uji.es>
2019-12-17 10:02:56 +0100

    Change style of cards

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -22,10 +22,12 @@ export default {
   margin-bottom: 24px;
   transition: all 0.2s linear;
   cursor: pointer;
+  border-radius: 12px;
 }
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+  background: rgba(4, 212, 240, 0.89)
 }
 .event-card > .title {
   margin: 0;
